88751b614b36146d9ee6dfa4dd6a2e82bc90cacf
ritvik1529 <ritvikgoyal300@gmail.com>
2025-06-28 19:05:39 +0530

    Add files via upload

---
+++ Basic Mathematical Operations.py
@@ -0,0 +1,10 @@
+a=int(input('Enter your first number:\t'))
+b=int(input('Enter your second number:\t'))
+add=a+b
+print('addition of give to numbers is: ',add)
+sub=a-b
+print('subtration of give to numbers is: ',sub)
+mul=a*b
+print('multiplication of give to numbers is: ',mul)
+div=a/b
+print('divition of give to numbers is: ',div)
---
+++ Personalized Greeting.py
@@ -0,0 +1,3 @@
+a=(input('Enter your first name:\t'))
+b=(input('Enter your last name:\t'))
+print('hello',a,b 'wellcome to python')
